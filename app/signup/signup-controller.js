2662cf494e9a963271c3379c431420e3317afeea
aramach8 <aramach8@hawk.iit.edu>
2017-07-10 15:28:03 -0500

    code for authentication, creating new user added and storing the user in db added

--- app/signup/signup-controller.js
+++ app/signup/signup-controller.js
@@ -1,6 +1,12 @@
 (function(){
     angular.module('TimeWaste')
-        .controller('SignupController', ['$scope', '$state', function($scope, $state){
-            
+        .controller('SignupController', ['$scope', '$state', '$http', function($scope, $state, $http){
+            $scope.createUser = function(){
+                $http.post('api/user/signup', $scope.newUser).then(function(response){
+                    
+                }).catch(function(error){
+                    console.log(error);
+                })
+            }
         }]);
 }());
--- server.js
+++ server.js
@@ -4,15 +4,22 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
+var authenticationController = require('./server/controllers/authentication-controller');
 
 mongoose.connect('mongodb://localhost:27017/time-waste');
 
+app.use(bodyParser.json());
+
+//declaring the base folder for the entire app
 app.use('/app', express.static(__dirname + "/app"));
 
 app.get('/', function(req, res){
    //res.sendFile('index.html'); 
    res.sendFile(path.resolve('index.html')); 
 });
+
+//Authentication
+app.post('/api/user/signup', authenticationController.signup);
 
 app.listen('3000', function(){
    console.log("Listening for localhost 3000"); 
