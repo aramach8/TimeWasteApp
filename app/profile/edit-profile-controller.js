d7656b8ddcb43174abd087cf0a4e0cb01fbc27b4
aramach8 <aramach8@hawk.iit.edu>
2017-07-10 22:39:25 -0500

    files for edit profile added with basic code

---
+++ app/profile/edit-profile-controller.js
@@ -0,0 +1,6 @@
+(function(){
+    angular.module('TimeWaste')
+    .controller('EditProfileController', ['Upload', '$scope', '$state', '$http', function(Upload, $scope, $state, $http){
+        
+    }]);
+}());
